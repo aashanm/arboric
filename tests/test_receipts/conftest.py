01fb965edcda4151e96e1d7ccb0d21353b223709
Aashan Meiyappan <aashan.meiyappan@uwaterloo.ca>
2026-03-19 10:09:05 -0700

    run importorskip before importing test modules

--- tests/test_receipts/conftest.py
+++ tests/test_receipts/conftest.py
@@ -2,12 +2,5 @@
 
 import pytest
 
-
-def pytest_collection_modifyitems(config, items):
-    """Skip all receipt tests if cryptography is not available."""
-    try:
-        import cryptography  # noqa: F401
-    except ImportError:
-        skip_marker = pytest.mark.skip(reason="cryptography not installed")
-        for item in items:
-            item.add_marker(skip_marker)
+# Skip all receipt tests if cryptography is not available
+pytest.importorskip("cryptography")
